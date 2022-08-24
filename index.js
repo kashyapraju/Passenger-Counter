161ede838c1b2bc206b089f333f75a23b4c785e4
Raj kashyap <98572611+kashyapraju@users.noreply.github.com>
2022-08-24 11:49:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+//console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.innerText += countStr 
+    //console.log(count)
+    countEl.innerText = 0
+}
